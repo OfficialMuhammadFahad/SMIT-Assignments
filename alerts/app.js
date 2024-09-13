bc7640862919d3ada08725b16cff792c70ada688
Muhammad Fahad <muhammadfahadbakkar143@gmail.com>
2024-09-13 16:48:22 +0500

    All work added

---
+++ alerts/app.js
@@ -0,0 +1,60 @@
+// Task 01 -- Write a script to greet your website visitor using JS alert box.
+alert("Hey Everyone!");
+
+// Task 02 -- Write a script to display following message on your web page:
+alert("Error! Please enter a valid password.");
+
+// Task 03 -- Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Task 04 -- Write a script to display following messages in sequence:
+// Welcome to JS Land..
+// Happy Coding! next line (Checkbox) Prevent this page from creating additional dialogs
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// Task 05 -- Generate the following message through browser’s developer console:
+// Image attach for the task 5
+
+// Task 06 -- Make use of alerts in your new/existing HTML & CSS project.
+// Task done in HTML and CSS
+
+// Task 07 Practice placement of <script></script> element in following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+
+// a. <script>alert("Done in head tag.")</script>;
+
+
+// b. 
+/* <body>
+    <script>
+    alert("Done in head tag before the pages tag.")
+</script>
+
+<h1>Alert</h1>
+</body> */
+
+
+// c. 
+/* <body>
+    <div>
+        <p>This is some content.</p>
+        <script>
+            alert("Inside the pages tag.")
+        </script>
+    </div>
+</body> */
+
+
+// d. 
+/* <body>
+  <!-- Here HTML content -->
+
+  <script>
+  alert("After the pages tag.");
+  </script>
+</body> */
